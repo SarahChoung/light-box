cb5333a16d559d623a03884baf9a7f514234397e
Sarah Choung <sarahjchoung@gmail.com>
2020-06-13 20:21:55 +0000

    Edited App class to include healthcheck

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -2,39 +2,35 @@ import React from 'react';
 import Header from './header';
 import ProductList from './product-list';
 import ProductDetails from './product-details';
-// export default class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       message: null,
-//       isLoading: true
-//     };
-//   }
-
-//   componentDidMount() {
-//     fetch('/api/health-check')
-//       .then(res => res.json())
-//       .then(data => this.setState({ message: data.message || data.error }))
-//       .catch(err => this.setState({ message: err.message }))
-//       .finally(() => this.setState({ isLoading: false }));
-//   }
-
-//   render() {
-//     return this.state.isLoading
-//       ? <h1>Testing connections...</h1>
-//       : <h1>{this.state.message}</h1>;
-//   }
-// }
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: null,
+      isLoading: true,
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/health-check')
+      .then(res => res.json())
+      .then(data => this.setState({ message: data.message || data.error }))
+      .catch(err => this.setState({ message: err.message }))
+      .finally(() => this.setState({ isLoading: false }));
+  }
 
   render() {
-    return (
-      <div>
+    return this.state.isLoading
+      ? <h1>Testing connections...</h1>
+      : <div>
         <Header />
         <ProductList />
         <ProductDetails />
-      </div>
-    );
+      </div>;
   }
 }
