be7de44b8bc5664fd81eb31af38ab697d236e644
Sarah Choung <sarahjchoung@gmail.com>
2020-07-10 00:01:37 +0000

    added image assets

---
+++ client/components/carousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className="carousel text-center">
+        <h1>hi</h1>
+      </div>
+    );
+  }
+}
