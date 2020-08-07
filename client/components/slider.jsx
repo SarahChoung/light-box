56b28d102031091c1f229a81db1fbb01cca3951b
Sarah Choung <sarahjchoung@gmail.com>
2020-08-07 04:46:16 +0000

    deleted gif files

--- client/components/slider.jsx
+++ client/components/slider.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 
 export default function Slider() {
-  const sliderArr = ['/images/iroh1.gif', '/images/iroh2.gif', '/images/slide3.gif', 4, 5];
+  // const sliderArr = ['/images/iroh1.gif', '/images/iroh2.gif', '/images/slide3.gif', '/images/slide3alt.gif', 5];
+  const sliderArr = ['/images/slide3.mp4', 5];
   const [x, setX] = useState(0);
   const goLeft = () => {
     x === 0 ? setX(-100 * (sliderArr.length - 1)) : setX(x + 100);
@@ -16,7 +17,9 @@ export default function Slider() {
         return (
           <div key={index} className="slide"
             style={{ transform: `translateX(${x}%)` }}>
-            <img className="banner-img" src={item}></img>
+            <video className="banner-img" width='100%' controls autoPlay>
+              <source src={item} type="video/mp4" />
+            </video>
           </div>
         );
       })}
